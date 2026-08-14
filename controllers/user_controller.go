f644b5be5b6c3fde415d6fced1c80489e0cd5e40
unknown <rahmatbaktiar9@gmail.com>
2026-08-14 09:33:31 +0700

    Add find, update & delete user, install cors

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -66,3 +66,133 @@ func CreateUser(c *gin.Context) {
 		},
 	})
 }
+
+func FindUserById(c *gin.Context) {
+	id := c.Param("id")
+
+	var user models.User
+
+	if err := database.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, structs.ErrorResponse{
+			Success: false,
+			Message: "User not found",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, structs.SuccessResponse{
+		Success: true,
+		Message: "User Found",
+		Data: structs.UserResponse{
+			Id:        user.Id,
+			Name:      user.Name,
+			Username:  user.Username,
+			Email:     user.Email,
+			Role:      user.Role,
+			IsActive:  true,
+			CreatedAt: user.CreatedAt.Format("2006-01-02 15:04:05"),
+			UpdatedAt: user.UpdatedAt.Format("2006-01-02 15:04:05"),
+		},
+	})
+}
+
+func UpdateUser(c *gin.Context) {
+	id := c.Param("id")
+
+	// 1. Cari user di database
+	var user models.User
+	if err := database.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, structs.ErrorResponse{
+			Success: false,
+			Message: "User Not Found",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	// 2. Bind Body JSON ke Request Struct
+	var req structs.UserUpdateRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, structs.ErrorResponse{
+			Success: false,
+			Message: "Validation Error",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	// 4. Assign nilai baru
+	user.Name = req.Name
+	user.Username = req.Username
+	user.Email = req.Email
+
+	if req.Role != "" {
+		user.Role = req.Role
+	}
+
+	// Cek nil pointer pada IsActive agar tidak panic
+	if req.IsActive != nil {
+		user.IsActive = *req.IsActive
+	}
+
+	// 5. Handle update password (jika dikirim oleh frontend)
+	if req.Password != "" {
+		user.Password = helpers.HashPassword(req.Password)
+	}
+
+	// 6. Simpan perubahan ke DB
+	if err := database.DB.Save(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, structs.ErrorResponse{
+			Success: false,
+			Message: "Internal Server Error",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	// 7. Response Sukses
+	c.JSON(http.StatusOK, structs.SuccessResponse{
+		Success: true,
+		Message: "User updated successfully",
+		Data: structs.UserResponse{
+			Id:        user.Id,
+			Name:      user.Name,
+			Username:  user.Username,
+			Email:     user.Email,
+			Role:      user.Role,
+			IsActive:  user.IsActive, // Menggunakan nilai dinamis dari database
+			CreatedAt: user.CreatedAt.Format("2006-01-02 15:04:05"),
+			UpdatedAt: user.UpdatedAt.Format("2006-01-02 15:04:05"),
+		},
+	})
+}
+
+func DeleteUser(c *gin.Context) {
+	id := c.Param("id")
+
+	var user models.User
+
+	if err := database.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, structs.ErrorResponse{
+			Success: false,
+			Message: "User not found",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	if err := database.DB.Delete(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, structs.ErrorResponse{
+			Success: false,
+			Message: "Internal Server Error",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, structs.SuccessResponse{
+		Success: true,
+		Message: "User deleted successfully",
+	})
+}
--- routes/route.go
+++ routes/route.go
@@ -4,16 +4,27 @@ import (
 	"projectmanagement/backend-api/controllers"
 	"projectmanagement/backend-api/middlewares"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:  []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+	}))
+
 	router.POST("/api/login", controllers.Login)
 
 	router.GET("/api/users", middlewares.AuthMiddleware(), controllers.GetAllUser)
 	router.POST("/api/users", middlewares.AuthMiddleware(), controllers.CreateUser)
+	router.GET("/api/users/:id", middlewares.AuthMiddleware(), controllers.FindUserById)
+	router.PUT("/api/users/:id", middlewares.AuthMiddleware(), controllers.UpdateUser)
+	router.DELETE("/api/users/:id", middlewares.AuthMiddleware(), controllers.DeleteUser)
 
 	return router
 }
